925988b710a80a2c21f0a13b355d93e15f721f40
KirillSmirnovff <smirovk.1990@gmail.com>
2021-10-04 15:06:36 +0300

    Revert "2. Сокобан. Тестовое задание. [#75283]"
    This reverts commit 0417b9fa1d99619ba3d02a9c4714dd88a7625e22.

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,35 +2,7 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean result = false;
-        for (int i = 0; i != 5; i++) {
-            if(board[i][i] == 1 && (horizontalCheck(board, i) || verticalCheck(board, i))) {
-                result = true;
-                break;
-            }
-        }
-        return result;
-    }
-
-    public static boolean horizontalCheck(int[][] board, int row) {
-        boolean result = true;
-        for (int cell = 0; cell != 5; cell++) {
-            if (board[row][cell] != 1) {
-                result = false;
-                break;
-            }
-        }
-        return  result;
-    }
-
-    public static boolean verticalCheck(int[][] board, int column) {
-        boolean result = true;
-        for (int cell = 0; cell != 5; cell++) {
-            if (board[cell][column] != 1) {
-                result = false;
-                break;
-            }
-        }
-        return  result;
+        boolean rsl = true;
+        return rsl;
     }
 }
